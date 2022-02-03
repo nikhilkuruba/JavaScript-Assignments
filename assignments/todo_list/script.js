b5d6c745456f82d825217145a8c3eb2db6074366
nikhilkuruba <nikhil.kuruba237@gmail.com>
2022-02-03 11:26:22 +0530

    Todos with  Actual Api

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -16,3 +16,31 @@ button.addEventListener('click',function(){
         
     }
 })
+
+let selector = document.getElementById('dropDown')
+selector.addEventListener("change",getTodoApi);
+function getTodoApi(){
+  fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json())
+  .then((data) =>{
+    if(selector.value == "completed"){
+      todoList.innerText = ""
+    data.forEach(item => {
+      if(item.completed == true){
+        var para = document.createElement('p');
+        para.innerText=item.title;
+        todoList.appendChild(para);
+      }}); 
+    }
+    else if(selector.value == "pending"){
+      todoList.innerText = ""
+    data.forEach(item => {
+      if(item.completed == false){
+        var para = document.createElement('p');
+        para.innerText=item.title;
+        todoList.appendChild(para);
+      }}); 
+    }
+
+  })
+}
